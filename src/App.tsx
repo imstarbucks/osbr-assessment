04fdbb7504ac9f33cbdbbea16c7ac0eb97b871f6
imstarbucks <37726091+imstarbucks@users.noreply.github.com>
2025-02-09 10:55:27 +0800

    chore: set up aliases path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import { Button } from '@components/button';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/button/index.tsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+  return <button>Button hwlo</button>;
+};
